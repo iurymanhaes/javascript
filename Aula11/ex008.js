ef88cafde65cc88b56be5b52b4d92f6802037ba1
iurymanhaes <iury.manhaes@gmail.com>
2020-06-25 13:07:31 -0300

    If/Else
    Condicional Simples e Composta

---
+++ Aula11/ex008.js
@@ -0,0 +1,10 @@
+ var vel = 12
+ console.log(`A velocidade do seu carro é ${vel} km/h`)
+ if(vel>60){//Condição simples
+    console.log('Você foi multado!')
+}
+ console.log('Dirija sempre usando o cinto de segurança!')
+
+
+    
+ 
