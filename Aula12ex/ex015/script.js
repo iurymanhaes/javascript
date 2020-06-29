573f45e2d1dbd9c598ab0989a8c266d6e5b2fc93
iurymanhaes <iury.manhaes@gmail.com>
2020-06-29 12:29:49 -0300

    carregando fotos

--- Aula12ex/ex015/script.js
+++ Aula12ex/ex015/script.js
@@ -7,9 +7,55 @@ function verificar(){
     if(fano.value.length == 0 || Number(fano.value) > ano){
         window.alert(`Verifique os dados e ene novamente!`)
     }else{
-        var fsex = window.document.getElementsByName(`radse`)
+        var fsex = window.document.getElementsByName(`radsex`)
+        var idade = ano - Number(fano.value)
+        var genero = ``
+        var img = window.document.createElement (`img`)
+        img.setAttribute('id','foto')
 
+        if(fsex[0].checked){
+            genero = `Homem`
+                if(idade >= 0 && idade <= 10){
+                    genero = 'Criança'   
+                    img.setAttribute('src','foto-bebe-masc.jpg')
 
+                }else if(idade < 21){
+                    genero = 'Adolescente'   
+                    img.setAttribute('src','foto-jovem-m.jpg')
+
+                }else if(idade < 50){
+                    genero = 'Adulto'   
+                    img.setAttribute('src','foto-adulto-m.jpg')
+
+                }else{
+                    genero = 'Idoso'   
+                    img.setAttribute('src','foto-idoso-m.jpg')
+
+                }                        
+        }else if(fsex[1].checked){
+            genero = `Mulher`
+            if(idade >= 0 && idade <= 10){
+                genero = 'Criança'   
+                img.setAttribute('src','foto-bebe-femi.jpg')
+
+            }else if(idade < 21){
+                genero = 'Adolescente'   
+                img.setAttribute('src','foto-jovem-f.jpg')
+
+            }else if(idade < 50){
+                genero = 'Adulta'   
+                img.setAttribute('src','foto-adulto-f.jpg')
+
+            }else{
+                genero = 'Idosa'   
+                img.setAttribute('src','foto-idoso-f.jpg')
+
+            }    
+        
+        }    
+        res.style.textAlign = `center`
+        res.innerHTML = (`Detectamos ${genero} com ${idade}`)
+        res.appendChild(img)
     }
 
 }
