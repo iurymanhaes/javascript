e9ecf60b46571dade4b6c51516a0181b798edc07
iurymanhaes <iury.manhaes@gmail.com>
2020-07-14 13:27:13 -0300

    repetições while e do - while
    aula 13 curso js , guanabara

---
+++ Aula13/do-while.js
@@ -0,0 +1,8 @@
+
+i = 1
+do{
+console.log(`Contando ${i}`)
+i++
+}while(i <=6)
+
+//Teste lógico no final
---
+++ Aula13/while.js
@@ -0,0 +1,19 @@
+var i = 1
+while(i <=6){
+    console.log(`Contadando ${i}`)
+    i++
+}
+
+//Teste lógico no início
+
+
+
+
+/*console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')*/
+
